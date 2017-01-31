896a000a5ed8e456df9322bb48e84cc499f0484a
Le-Yo <le-yo@le-yo.com>
2017-01-31 18:15:25 +0300

    Added routes for API

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,17 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+Route::group(['middleware' => 'auth:api'], function () {
+
+    // Voters
+    Route::group(['prefix' => 'TechAnalysis'], function () {
+        Route::post('request', ['as' => 'techanalysis.request', 'uses' => 'Api\TechAnalysisController@request']);
+        Route::get('voted', ['as' => 'voters.voted', 'uses' => 'Api\VotersApiController@listVotersWhoHaveVoted']);
+        Route::get('not-voted', ['as' => 'voters.not-voted', 'uses' => 'Api\VotersApiController@listVotersWhoHaveNotVoted']);
+        Route::post('mark-voted', ['as' => 'voters.mark-voted', 'uses' => 'Api\VotersApiController@markAsVoted']);
+        Route::post('send-reminder', ['as' => 'voters.send-reminder', 'uses' => 'Api\VotersApiController@sendReminderToVoter']);
+    });
+
+});
