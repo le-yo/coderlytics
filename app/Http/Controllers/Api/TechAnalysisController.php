2281c089a151ab6ab8d03818c8eee22fe07526d2
Le-Yo <le-yo@le-yo.com>
2017-01-31 18:29:45 +0300

    now working

--- app/Http/Controllers/Api/TechAnalysisController.php
+++ app/Http/Controllers/Api/TechAnalysisController.php
@@ -29,7 +29,6 @@ class TechAnalysisController extends Controller
             return Response::create(['error' => $validator->errors(), 'code' => 422, 'message' => 'Missing field values'], 422);
         }
 
-        $voter = Voter::where('id', '=', $request->input('user_id'))->where('phone', '=', $request->input('phone'))->first();
 
         return Response::create(['success'=>1,'message'=>'Received new repo to score','github_repo_url' => $request->input('github_repo_url'), 'code' => 200], 200);
     }
