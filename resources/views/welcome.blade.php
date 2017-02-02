0b4f32ce0d0a41bbd2a9ff5fe7493090a9369360
Le-Yo <le-yo@le-yo.com>
2017-02-02 14:38:25 +0300

    changed from coderlytics to unleash

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Coderlytics
+                    Unleash
                 </div>
 
                 <div class="links">
